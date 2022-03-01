6559b23a3503243cac848bcfe9968a155a61cb6b
Nazar <nazar383875@gmail.com>
2022-03-01 18:53:24 +0300

    butter

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var buttonTrafic: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = 66
+        redView.alpha = 0.3
+        
+        yellowView.layer.cornerRadius = 66
+        yellowView.alpha = 0.3
+        
+        greenView.layer.cornerRadius = 66
+        greenView.alpha = 0.3
+        
+        buttonTrafic.layer.cornerRadius = 10
     }
 
 
